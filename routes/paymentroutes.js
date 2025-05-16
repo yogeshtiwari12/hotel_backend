00418b86fb64e63dc468a0b264438c27bad71bc7
Yogesh Tiwari <119746250+yogeshtiwari12@users.noreply.github.com>
2025-05-16 17:18:58 +0530

    Update paymentroutes.js

--- routes/paymentroutes.js
+++ routes/paymentroutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { createPayment, savePayment } from '../methods/paymentmethod.js';
 import { createRazorpayOrder, verifyPayment } from '../methods/razorpaymethod.js';
 
 const router = express.Router();
